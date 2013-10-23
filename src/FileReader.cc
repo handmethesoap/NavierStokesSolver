0a4790b0d9a612ed54be489fb4ef2a7f88bf6503
handmethesoap <samnees@gmail.com>
2013-10-23 13:20:12 +0200

    setup commit

---
+++ src/FileReader.cc
@@ -0,0 +1,49 @@
+
+#include <FileReader.hh>
+
+
+void FileReader::registerIntParameter(const std::string &key, int init)
+{
+   //TODO
+}
+
+void FileReader::registerRealParameter(const std::string &key, real init)
+{
+   //TODO
+}
+
+void FileReader::registerStringParameter(const std::string &key, const std::string &init)
+{
+   //TODO
+}
+
+void FileReader::setParameter(const std::string &key, const std::string &in)
+{
+   //TODO
+}
+
+void FileReader::setParameter(const std::string &key, real in)
+{
+   //TODO
+}
+
+void FileReader::setParameter(const std::string &key, int in)
+{
+   //TODO
+}
+
+
+bool FileReader::readFile(const std::string &name)
+{
+   //TODO
+   return false;
+}
+
+
+
+void FileReader::printParameters() const
+{
+   //TODO
+}
+
+
---
+++ src/main.cc
@@ -0,0 +1,11 @@
+#include "Array.hh"
+#include "FileReader.hh"
+
+#include <iostream>
+
+
+int main( int argc, char** argv )
+{
+
+   return 0;
+}
