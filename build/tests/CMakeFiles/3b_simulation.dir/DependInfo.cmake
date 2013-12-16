714c44ae961537f4d780b1ef3e38c6d204cbbd5b
handmethesoap <samnees@gmail.com>
2013-12-16 17:17:45 +0100

    sum over rhs check added to sor solver.  small changes made to parameter names

---
+++ build/tests/CMakeFiles/3b_simulation.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/auto-home/stud/zo99vyha/Documents/NuSiF/Assignment4/NavierStokesSolver/tests/3b_simulation.cc" "/auto-home/stud/zo99vyha/Documents/NuSiF/Assignment4/NavierStokesSolver/build/tests/CMakeFiles/3b_simulation.dir/3b_simulation.cc.o"
+  "/auto-home/stud/zo99vyha/Documents/NuSiF/Assignment4/NavierStokesSolver/src/Array.cc" "/auto-home/stud/zo99vyha/Documents/NuSiF/Assignment4/NavierStokesSolver/build/tests/CMakeFiles/3b_simulation.dir/__/src/Array.cc.o"
+  "/auto-home/stud/zo99vyha/Documents/NuSiF/Assignment4/NavierStokesSolver/src/Debug.cc" "/auto-home/stud/zo99vyha/Documents/NuSiF/Assignment4/NavierStokesSolver/build/tests/CMakeFiles/3b_simulation.dir/__/src/Debug.cc.o"
+  "/auto-home/stud/zo99vyha/Documents/NuSiF/Assignment4/NavierStokesSolver/src/FileReader.cc" "/auto-home/stud/zo99vyha/Documents/NuSiF/Assignment4/NavierStokesSolver/build/tests/CMakeFiles/3b_simulation.dir/__/src/FileReader.cc.o"
+  "/auto-home/stud/zo99vyha/Documents/NuSiF/Assignment4/NavierStokesSolver/src/FluidSimulator.cc" "/auto-home/stud/zo99vyha/Documents/NuSiF/Assignment4/NavierStokesSolver/build/tests/CMakeFiles/3b_simulation.dir/__/src/FluidSimulator.cc.o"
+  "/auto-home/stud/zo99vyha/Documents/NuSiF/Assignment4/NavierStokesSolver/src/SORSolver.cc" "/auto-home/stud/zo99vyha/Documents/NuSiF/Assignment4/NavierStokesSolver/build/tests/CMakeFiles/3b_simulation.dir/__/src/SORSolver.cc.o"
+  "/auto-home/stud/zo99vyha/Documents/NuSiF/Assignment4/NavierStokesSolver/src/StaggeredGrid.cc" "/auto-home/stud/zo99vyha/Documents/NuSiF/Assignment4/NavierStokesSolver/build/tests/CMakeFiles/3b_simulation.dir/__/src/StaggeredGrid.cc.o"
+  "/auto-home/stud/zo99vyha/Documents/NuSiF/Assignment4/NavierStokesSolver/src/VTKWriter.cc" "/auto-home/stud/zo99vyha/Documents/NuSiF/Assignment4/NavierStokesSolver/build/tests/CMakeFiles/3b_simulation.dir/__/src/VTKWriter.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../tests/../src"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/tests/CMakeFiles/obstacle_test.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/obstacle_test.dir/__/src/FileReader.cc.o"
+  "CMakeFiles/obstacle_test.dir/__/src/Debug.cc.o"
+  "CMakeFiles/obstacle_test.dir/__/src/FluidSimulator.cc.o"
+  "CMakeFiles/obstacle_test.dir/__/src/Array.cc.o"
+  "CMakeFiles/obstacle_test.dir/__/src/StaggeredGrid.cc.o"
+  "CMakeFiles/obstacle_test.dir/__/src/SORSolver.cc.o"
+  "CMakeFiles/obstacle_test.dir/__/src/VTKWriter.cc.o"
+  "CMakeFiles/obstacle_test.dir/ObstacleTest.cc.o"
+  "obstacle_test.pdb"
+  "obstacle_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/obstacle_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
